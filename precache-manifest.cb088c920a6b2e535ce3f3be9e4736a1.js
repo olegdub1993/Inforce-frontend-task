68575384f437d89f2b9390d0b5f00d108ccf363f
Oleg Dub <olegdub155@gmail.com>
2021-10-12 17:34:25 +0200

    Updates

---
+++ precache-manifest.cb088c920a6b2e535ce3f3be9e4736a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d231b2105b797af044d4d6288395868",
+    "url": "/Inforce-frontend-task/index.html"
+  },
+  {
+    "revision": "5f8d6ba31a8767324566",
+    "url": "/Inforce-frontend-task/static/css/main.a3462b61.chunk.css"
+  },
+  {
+    "revision": "0b8ec07d9de068d117e7",
+    "url": "/Inforce-frontend-task/static/js/2.3db966cc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Inforce-frontend-task/static/js/2.3db966cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f8d6ba31a8767324566",
+    "url": "/Inforce-frontend-task/static/js/main.03573cea.chunk.js"
+  },
+  {
+    "revision": "4422050db2a3f63fd9ef",
+    "url": "/Inforce-frontend-task/static/js/runtime-main.23b8c9c1.js"
+  }
+]);
